4a164fcfac86761299f746d7fa503c9ea957cb9c
Taekyu Lim <dualcoder.pe@gmail.com>
2022-06-20 11:16:08 +0900

    fix export code to comment

--- domain/lib/domain.dart
+++ domain/lib/domain.dart
@@ -3,10 +3,10 @@
 /// More dartdocs go here.
 // library domain;
 
-export 'src/data/model/order.dart';
-export 'src/data/model/order_result.dart';
-export 'src/data/model/product.dart';
-export 'src/data/repository/order_repository.dart';
-export 'src/presentation/bloc/order_bloc.dart';
-export 'src/usecase/send_order_usecase.dart';
+// export 'src/data/model/order.dart';
+// export 'src/data/model/order_result.dart';
+// export 'src/data/model/product.dart';
+// export 'src/data/repository/order_repository.dart';
+// export 'src/presentation/bloc/order_bloc.dart';
+// export 'src/usecase/send_order_usecase.dart';
 
